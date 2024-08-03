425e6e0bb3ccb72803f6ccefb004fbdce433db0a
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2024-08-03 17:42:19 +0100

    update tests

--- tests/toHaveProfanity.php
+++ tests/toHaveProfanity.php
@@ -7,14 +7,14 @@ use Pest\Arch\Exceptions\ArchExpectationFailedException;
 it('fails if a file contains profanity in a comment', function () {
     expect('Tests\Fixtures\HasProfanityInComment')
         ->toHaveNoProfanity();
-})->throws(ArchExpectationFailedException::class, "Expecting 'tests/Fixtures/HasProfanityInComment.php' to not use profanity.");
+})->throws(ArchExpectationFailedException::class);
 
 it('fails if a file contains profanity in a method', function () {
     expect('Tests\Fixtures\HasProfanityInMethod')
         ->toHaveNoProfanity();
-})->throws(ArchExpectationFailedException::class, "Expecting 'tests/Fixtures/HasProfanityInMethod.php' to not use profanity.");
+})->throws(ArchExpectationFailedException::class);
 
 it('fails if a file contains profanity in a property', function () {
     expect('Tests\Fixtures\HasProfanityInProperty')
         ->toHaveNoProfanity();
-})->throws(ArchExpectationFailedException::class, "Expecting 'tests/Fixtures/HasProfanityInProperty.php' to not use profanity.");
+})->throws(ArchExpectationFailedException::class);
