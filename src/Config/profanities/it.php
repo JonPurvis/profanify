e4567ea2cd8c6c8f5632f5d01bd1566f1c15a7d7
Davide Prevosto <davide.prevosto@encodia.it>
2024-08-16 11:34:53 +0200

    chore: rename words folder to profanities

--- src/Config/profanities/it.php
+++ src/Config/profanities/it.php
@@ -3,11 +3,16 @@
 declare(strict_types=1);
 
 return [
+    'cazzo',
     'dioporco',
     'dio porco',
     'diocane',
     'dio cane',
     'dio can', // Veneto's version
+    'porcamadonna',
+    'porca madonna',
     'porcodio',
     'porco dio',
+    'stocazzo',
+    'vaffanculo',
 ];
