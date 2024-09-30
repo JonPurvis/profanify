cb0217789073a63f0e22cc058b41edd7aaa217f3
Abdellah Boutmad <148339843+AbdellahBoutmad@users.noreply.github.com>
2024-09-30 23:34:40 +0100

    add arabic words (#17)
    I have add new words in arabic like English and Italy

---
+++ src/Config/profanities/ar.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+// Please ensure all words in this file are lowercase
+
+return [
+    'عاهرة',
+    'قواد',
+    'قواد',
+    'ملاوي',
+    'الخرا',
+    'خرية',
+    'خرا',
+    'قود',
+    'سير تقود',
+    'وجه الخرا',
+    'خاري',
+    'خارية',
+    'اللاذينا',
+    'زانية',
+    'احبة',
+    'دور تقود',
+    'مقود',
+    'الملاوي',
+    'lkhra',
+    'kahba',
+    'lmlawi',
+    '9owd',
+    'sir trawd',
+    'dor t9awd',
+    'wjh lkhra',
+    '3ahira',
+    'lmlawi',
+];
