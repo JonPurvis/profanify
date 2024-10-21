f2ef76669fa25e4b92ab33bacb7f58c2d60ac32b
Sean Poynter-Smith <spoyntersmith@users.noreply.github.com>
2024-10-22 00:37:29 +0100

    Add found words to output (#26)
    Closes #25
    
    Output found words to the terminal message so it reads:
    `"Expected File.php to not have profanity, but found: 'x, y, z'"`

--- src/Expectations/Profanity.php
+++ src/Expectations/Profanity.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
+use JonPurvis\Profanify\Expectations\TargetedProfanity;
 use Pest\Arch\Contracts\ArchExpectation;
-use Pest\Arch\Expectations\Targeted;
 use Pest\Arch\Support\FileLineFinder;
 use PHPUnit\Architecture\Elements\ObjectDescription;
 
-expect()->extend('toHaveNoProfanity', fn (array $excluding = [], array $including = [], $language = null): ArchExpectation => Targeted::make(
+expect()->extend('toHaveNoProfanity', fn (array $excluding = [], array $including = [], $language = null): ArchExpectation => TargetedProfanity::make(
     $this,
     function (ObjectDescription $object) use (&$foundWords, $excluding, $including, $language): bool {
 
@@ -50,7 +50,9 @@ expect()->extend('toHaveNoProfanity', fn (array $excluding = [], array $includin
 
         return $foundWords === [];
     },
-    'to not use profanity',
+    function ($path) use (&$foundWords): string {
+        return "to have no profanity, but found '".implode(', ', array_values($foundWords ?? []))."'";
+    },
     FileLineFinder::where(function (string $line) use (&$foundWords): bool {
         return str_contains(strtolower($line), strtolower((string) array_values($foundWords ?? [])[0]));
     })
