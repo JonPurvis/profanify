00ef3c6d3445c7844184c663894d332980096574
Matthieu Lempereur <matt.lempereur@gmail.com>
2025-02-27 02:18:13 +0100

    add `assert` to tolerated list (#42)
    I think `assert` can be a common word in codebase 
    
    I've also added a new test that I thought would be useful here

---
+++ tests/Fixtures/HasWordStartingWithToleratedProfanity.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures;
+
+class HasWordStartingWithToleratedProfanity
+{
+    // Make an assertion
+    public function assertThis(): void
+    {
+        //
+    }
+}
