b6572772d4243a53b39d332a7d99488d6020bb7c
Faissal Wahabali <fwahabali@gmail.com>
2025-05-06 17:03:32 +0100

    Ordered config files (#47)
    In this PR I make sure config files content is ordered alphabetically,
    and added a test to ensure that any future content is always ordered.

--- tests/configFiles.php
+++ tests/configFiles.php
@@ -3,5 +3,8 @@
 declare(strict_types=1);
 
 test('config files return lowercase and unique values', function () {
-    expect('src/Config')->toReturnLowercase()->toReturnUnique();
+    expect('src/Config')
+        ->toReturnLowercase()
+        ->toReturnUnique()
+        ->toBeOrdered();
 });
